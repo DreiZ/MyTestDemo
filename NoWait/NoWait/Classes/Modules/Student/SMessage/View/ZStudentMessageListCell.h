366eb4cb9b429f2a3feaa185e703fc27b21b0196
zzz <zhang8520322@sina.com>
2020-01-13 14:05:04 +0800

    消息列表

---
+++ NoWait/NoWait/Classes/Modules/Student/SMessage/View/ZStudentMessageListCell.h
@@ -0,0 +1,14 @@
+//
+//  ZStudentMessageListCell.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/1/13.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZBaseCell.h"
+
+@interface ZStudentMessageListCell : ZBaseCell
+
+@end
+
--- NoWait/NoWait/Classes/Modules/Student/SMessage/ViewController/ZStudentMessageVC.m
+++ NoWait/NoWait/Classes/Modules/Student/SMessage/ViewController/ZStudentMessageVC.m
@@ -7,11 +7,18 @@
 //
 
 #import "ZStudentMessageVC.h"
+#import "ZCellConfig.h"
 
-@interface ZStudentMessageVC ()
+#import "ZSpaceEmptyCell.h"
+#import "ZStudentMessageListCell.h"
 
+
+@interface ZStudentMessageVC ()<UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic,strong) UITableView *iTableView;
+
+@property (nonatomic,strong) NSMutableArray *dataSources;
+@property (nonatomic,strong) NSMutableArray *cellConfigArr;
 @end
-
 @implementation ZStudentMessageVC
 
 
@@ -25,17 +32,126 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setNavigation];
+    [self setDataSource];
+    [self initCellConfigArr];
+    [self setupMainView];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setDataSource {
+    _dataSources = @[].mutableCopy;
+    _cellConfigArr = @[].mutableCopy;
+    
+    [self initCellConfigArr];
 }
-*/
 
+- (void)initCellConfigArr {
+//    ZCellConfig *spacCellConfig = [ZCellConfig cellConfigWithClassName:[ZSpaceEmptyCell className] title:[ZSpaceEmptyCell className] showInfoMethod:@selector(setBackColor:) heightOfCell:CGFloatIn750(8) cellType:ZCellTypeClass dataModel:KBackColor];
+//    [self.cellConfigArr addObject:spacCellConfig];
+    
+    ZCellConfig *messageCellConfig = [ZCellConfig cellConfigWithClassName:[ZStudentMessageListCell className] title:[ZStudentMessageListCell className] showInfoMethod:nil heightOfCell:[ZStudentMessageListCell z_getCellHeight:nil] cellType:ZCellTypeClass dataModel:nil];
+    [self.cellConfigArr addObject:messageCellConfig];
+}
+
+
+- (void)setNavigation {
+    self.isHidenNaviBar = NO;
+    [self.navigationItem setTitle:@"消息"];
+}
+
+- (void)setupMainView {
+    [self.view addSubview:self.iTableView];
+    [_iTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.view);
+        make.bottom.equalTo(self.view.mas_bottom);
+        make.top.equalTo(self.view.mas_top).offset(10);
+    }];
+}
+
+#pragma mark lazy loading...
+-(UITableView *)iTableView {
+    if (!_iTableView) {
+        _iTableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _iTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _iTableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+        if ([_iTableView respondsToSelector:@selector(contentInsetAdjustmentBehavior)]) {
+            _iTableView.estimatedRowHeight = 0;
+            _iTableView.estimatedSectionHeaderHeight = 0;
+            _iTableView.estimatedSectionFooterHeight = 0;
+            if (@available(iOS 11.0, *)) {
+                _iTableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+            } else {
+                // Fallback on earlier versions
+            }
+        } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            self.automaticallyAdjustsScrollViewInsets = NO;
+#pragma clang diagnostic pop
+        } _iTableView.delegate = self;
+        _iTableView.dataSource = self;
+    }
+    return _iTableView;
+}
+
+#pragma mark tableView -------datasource-----
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _cellConfigArr.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ZCellConfig *cellConfig = [_cellConfigArr objectAtIndex:indexPath.row];
+    ZBaseCell *cell;
+    cell = (ZBaseCell*)[cellConfig cellOfCellConfigWithTableView:tableView dataModel:cellConfig.dataModel];
+    if ([cellConfig.title isEqualToString:@"ZSpaceEmptyCell"]){
+//        ZSpaceEmptyCell *enteryCell = (ZSpaceEmptyCell *)cell;
+        
+    }
+    return cell;
+}
+
+#pragma mark tableView ------delegate-----
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ZCellConfig *cellConfig = _cellConfigArr[indexPath.row];
+    CGFloat cellHeight =  cellConfig.heightOfCell;
+    return cellHeight;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.01f;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.01f;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    ZCellConfig *cellConfig = [_cellConfigArr objectAtIndex:indexPath.row];
+    if ([cellConfig.title isEqualToString:@"ZSpaceCell"]) {
+        
+    }
+}
+
+#pragma mark vc delegate-------------------
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
 @end
+
