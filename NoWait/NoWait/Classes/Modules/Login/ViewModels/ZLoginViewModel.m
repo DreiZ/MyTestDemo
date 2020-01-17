af086f35b5791417ce8a08ee024300200059f4ae
zzz <zhang8520322@sina.com>
2020-01-17 16:48:52 +0800

    网络调整

--- NoWait/NoWait/Classes/Modules/Login/ViewModels/ZLoginViewModel.m
+++ NoWait/NoWait/Classes/Modules/Login/ViewModels/ZLoginViewModel.m
@@ -59,7 +59,7 @@
 //        
 //    }];
 //    return;
-    [ZNetworkingManager postServerType:ZServerTypeCode url:URL_sms_v1_captcha params:@{@"identifier":@"graphic"} completionHandler:^(id data, NSError *error) {
+    [ZNetworkingManager postServerType:ZServerTypeCode url:URL_sms_v1_captcha params:@{} completionHandler:^(id data, NSError *error) {
         DLog(@"return login code %@", data);
         block(YES,data);
 //        if (data && [data isKindOfClass:[NSDictionary class]]) {
