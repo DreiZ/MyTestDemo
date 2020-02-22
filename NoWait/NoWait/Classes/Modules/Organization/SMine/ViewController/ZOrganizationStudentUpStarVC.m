dbc43cb3b04d1928d701b6a5b1d052174fe6a99e
zzz <zhang8520322@sina.com>
2020-02-22 16:53:34 +0800

    学员风采

---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationStudentUpStarVC.m
@@ -0,0 +1,32 @@
+//
+//  ZOrganizationStudentUpStarVC.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/2/22.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZOrganizationStudentUpStarVC.h"
+
+@interface ZOrganizationStudentUpStarVC ()
+
+@end
+
+@implementation ZOrganizationStudentUpStarVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
