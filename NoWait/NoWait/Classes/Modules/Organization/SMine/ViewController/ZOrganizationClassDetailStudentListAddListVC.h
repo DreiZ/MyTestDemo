40a150854b003175ce6bc3bad3ccfbfb3251a023
zzz <zhang8520322@sina.com>
2020-02-26 16:28:21 +0800

    课程添加学员

---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationClassDetailStudentListAddListVC.h
@@ -0,0 +1,17 @@
+//
+//  ZOrganizationClassDetailStudentListAddListVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/2/26.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZOrganizationClassDetailStudentListAddListVC : ZViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationClassDetailStudentListAddVC.h
@@ -0,0 +1,17 @@
+//
+//  ZOrganizationClassDetailStudentListAddVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/2/26.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZOrganizationClassDetailStudentListAddVC : ZViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
