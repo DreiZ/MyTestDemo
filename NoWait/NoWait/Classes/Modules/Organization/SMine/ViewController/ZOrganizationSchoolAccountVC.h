58837a8a6c75760b2bfb8e2293748ea1cc650f4a
zzz <zhang8520322@sina.com>
2020-02-26 17:41:51 +0800

    zhanghu

---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationSchoolAccountVC.h
@@ -0,0 +1,17 @@
+//
+//  ZOrganizationSchoolAccountVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/2/26.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZOrganizationSchoolAccountVC : ZViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
