7b343242a42e4726586faacff0054d0cdd3030a0
zzz <zhang8520322@sina.com>
2020-03-03 15:48:57 +0800

    修改密码

---
+++ NoWait/NoWait/Classes/Modules/Student/SMine/View/settting/ZMineAccountTextFieldCell.h
@@ -0,0 +1,17 @@
+//
+//  ZMineAccountTextFieldCell.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/3.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZBaseCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMineAccountTextFieldCell : ZBaseCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NoWait/NoWait/Classes/Modules/Student/SMine/ViewController/ZAccountChangePasswordVC.h
@@ -0,0 +1,17 @@
+//
+//  ZAccountChangePasswordVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/3.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZTableViewViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZAccountChangePasswordVC : ZTableViewViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
