9fab683e8e3a644d344112ba3c3858c9b1db8755
zzz <zhang8520322@sina.com>
2020-03-21 16:17:49 +0800

    购买课程

---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/View/lessonBuyView/ZStudentLessonTeacherCell.h
@@ -0,0 +1,17 @@
+//
+//  ZStudentLessonTeacherCell.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZBaseCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZStudentLessonTeacherCell : ZBaseCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/View/organizationDetail/ZStudentLessonSelectMainNewView.h
@@ -0,0 +1,17 @@
+//
+//  ZStudentLessonSelectMainNewView.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZStudentLessonSelectMainNewView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
