874c7f70eec94c4a5665cd7258cd3ce7b8b76eb3
zzz <zhang8520322@sina.com>
2020-03-21 15:15:20 +0800

    课程详情

---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/View/organizationDetail/ZStudentOrganizationDetailEnteryCell.m
@@ -0,0 +1,24 @@
+//
+//  ZStudentOrganizationDetailEnteryCell.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZStudentOrganizationDetailEnteryCell.h"
+
+@implementation ZStudentOrganizationDetailEnteryCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/ViewController/ZStudentLessonDetailVC.h
@@ -0,0 +1,17 @@
+//
+//  ZStudentLessonDetailVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZTableViewViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZStudentLessonDetailVC : ZTableViewViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
