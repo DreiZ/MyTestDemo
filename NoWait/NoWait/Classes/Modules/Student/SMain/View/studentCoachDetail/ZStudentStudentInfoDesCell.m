cd739f4414b872ec611d389925683244fe58834d
zzz <zhang8520322@sina.com>
2020-03-21 12:05:01 +0800

    学员详情

---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/View/studentCoachDetail/ZStudentStudentInfoDesCell.m
@@ -0,0 +1,24 @@
+//
+//  ZStudentStudentInfoDesCell.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZStudentStudentInfoDesCell.h"
+
+@implementation ZStudentStudentInfoDesCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/ViewController/ZStudentStudentDetailVC.h
@@ -0,0 +1,17 @@
+//
+//  ZStudentStudentDetailVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/21.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZTableViewViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZStudentStudentDetailVC : ZTableViewViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
