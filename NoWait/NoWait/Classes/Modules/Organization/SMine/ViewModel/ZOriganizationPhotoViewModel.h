6b1188af4aa8f0961d9b82f61712d8bba0aacc6c
zzz <zhang8520322@sina.com>
2020-03-23 17:55:01 +0800

    图片管理

---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewModel/ZOriganizationPhotoViewModel.h
@@ -0,0 +1,17 @@
+//
+//  ZOriganizationPhotoViewModel.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/23.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZOriganizationPhotoViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewModel/ZOriganizationPhotoViewModel.m
@@ -0,0 +1,13 @@
+//
+//  ZOriganizationPhotoViewModel.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/23.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZOriganizationPhotoViewModel.h"
+
+@implementation ZOriganizationPhotoViewModel
+
+@end
