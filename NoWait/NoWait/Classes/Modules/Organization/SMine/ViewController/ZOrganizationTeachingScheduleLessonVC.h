b8e7e57408a58221c1388af0fcf387e6127128e2
zzz <zhang8520322@sina.com>
2020-03-23 15:21:33 +0800

    排课

---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationTeachingScheduleLessonVC.h
@@ -0,0 +1,17 @@
+//
+//  ZOrganizationTeachingScheduleLessonVC.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/23.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZTableViewViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZOrganizationTeachingScheduleLessonVC : ZTableViewViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationTeachingScheduleLessonVC.m
@@ -0,0 +1,32 @@
+//
+//  ZOrganizationTeachingScheduleLessonVC.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/23.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZOrganizationTeachingScheduleLessonVC.h"
+
+@interface ZOrganizationTeachingScheduleLessonVC ()
+
+@end
+
+@implementation ZOrganizationTeachingScheduleLessonVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
