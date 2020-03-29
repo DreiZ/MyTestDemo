c6bc7a41ba063f41affd6dcf31925c6a9eada10a
zzz <zhang8520322@sina.com>
2020-03-29 17:51:10 +0800

    课程评价  搜索课程

---
+++ NoWait/NoWait/Classes/Modules/Student/SMain/View/studentCoachDetail/ZStudentEvaListReEvaCell.m
@@ -0,0 +1,24 @@
+//
+//  ZStudentEvaListReEvaCell.m
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/3/29.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZStudentEvaListReEvaCell.h"
+
+@implementation ZStudentEvaListReEvaCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
