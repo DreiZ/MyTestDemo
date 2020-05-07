0bad69d127623ca5d813b8ea36a926926e012103
zzz <zhang8520322@sina.com>
2020-05-07 17:05:09 +0800

    网易云信集成

---
+++ NoWait/NoWait/Classes/Modules/Student/SMessage/ViewController/ZSessionListViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZSessionListViewController.h
+//  NoWait
+//
+//  Created by zhuang zhang on 2020/5/7.
+//  Copyright © 2020 zhuang zhang. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZSessionListViewController : ZViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
