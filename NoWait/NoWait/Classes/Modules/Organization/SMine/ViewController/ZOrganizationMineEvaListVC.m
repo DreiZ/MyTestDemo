573fef7bf38dfb72307b3b4bb84d530e75a5cc5a
zzz <zhang8520322@sina.com>
2020-06-04 17:49:38 +0800

    页面调整

--- NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationMineEvaListVC.m
+++ NoWait/NoWait/Classes/Modules/Organization/SMine/ViewController/ZOrganizationMineEvaListVC.m
@@ -55,7 +55,7 @@
     [self.iTableView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.right.equalTo(self.view);
         make.bottom.equalTo(self.view.mas_bottom);
-        make.top.equalTo(self.view.mas_top).offset(10);
+        make.top.equalTo(self.view.mas_top).offset(0);
     }];
 }
 - (NSMutableDictionary *)param {
