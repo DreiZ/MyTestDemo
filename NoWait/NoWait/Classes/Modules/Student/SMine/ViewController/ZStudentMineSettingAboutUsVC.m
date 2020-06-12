60c457de3f611888e665554f4df9725c7e958985
zzz <zhang8520322@sina.com>
2020-06-12 11:37:19 +0800

    关于我们调整

--- NoWait/NoWait/Classes/Modules/Student/SMine/ViewController/ZStudentMineSettingAboutUsVC.m
+++ NoWait/NoWait/Classes/Modules/Student/SMine/ViewController/ZStudentMineSettingAboutUsVC.m
@@ -17,49 +17,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self setNavigation];
-    [self setTableViewGaryBack];
-    [self initCellConfigArr];
-    [self.iTableView reloadData];
-    self.iTableView.tableHeaderView = self.topView;
-    
-    [ZPublicTool settingCheckUpdateVersion];
-}
-
-
-- (void)initCellConfigArr {
-    [super initCellConfigArr];
-    
-    NSString *temp = @"1.0.0";
-    NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
-    if ([infoDictionary objectForKey:@"CFBundleShortVersionString"]) {
-        temp = [NSString stringWithFormat:@"v%@",[infoDictionary objectForKey:@"CFBundleShortVersionString"]];
-    }
-
-    NSArray <NSArray *>*titleArr = @[@[@"当前版本", temp, @"version"],@[@"微信公众号", @"xiangcenter", @"weixin"]];
-
-    for (int i = 0; i < titleArr.count; i++) {
-        ZBaseSingleCellModel *model = [[ZBaseSingleCellModel alloc] init];
-        model.leftTitle = titleArr[i][0];
-        model.rightTitle = titleArr[i][1];
-        model.leftFont = [UIFont fontContent];
-        model.cellHeight = CGFloatIn750(110);
-        model.cellTitle = titleArr[i][2];
-
-        ZCellConfig *menuCellConfig = [ZCellConfig cellConfigWithClassName:[ZSingleLineCell className] title:model.cellTitle showInfoMethod:@selector(setModel:) heightOfCell:[ZSingleLineCell z_getCellHeight:nil] cellType:ZCellTypeClass dataModel:model];
-        [self.cellConfigArr addObject:menuCellConfig];
-
-        if (i == 2) {
-            ZCellConfig *topCellConfig = [ZCellConfig cellConfigWithClassName:[ZSpaceEmptyCell className] title:[ZSpaceEmptyCell className] showInfoMethod:@selector(setBackColor:) heightOfCell:CGFloatIn750(20) cellType:ZCellTypeClass dataModel:adaptAndDarkColor([UIColor colorGrayBG], [UIColor colorGrayBGDark])];
-            [self.cellConfigArr addObject:topCellConfig];
-        }
-    }
-}
-
-- (void)setNavigation {
     self.isHidenNaviBar = NO;
-    [self.navigationItem setTitle:@"关于"];
+    
+    __weak typeof(self) weakSelf = self;
+    self.zChain_setNavTitle(@"关于").zChain_setTableViewGary();
+    self.zChain_resetMainView(^{
+        self.iTableView.tableHeaderView = self.topView;
+    }).zChain_block_setUpdateCellConfigData(^(void (^update)(NSMutableArray *)) {
+        [weakSelf.cellConfigArr removeAllObjects];
+        NSString *temp = @"1.0.0";
+        NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+        if ([infoDictionary objectForKey:@"CFBundleShortVersionString"]) {
+            temp = [NSString stringWithFormat:@"v%@",[infoDictionary objectForKey:@"CFBundleShortVersionString"]];
+        }
+
+        NSArray <NSArray *>*titleArr = @[@[@"当前版本", temp, @"version"],@[@"微信公众号", @"xiangcenter", @"weixin"]];
+
+        for (int i = 0; i < titleArr.count; i++) {
+            ZLineCellModel *model =  ZLineCellModel.zz_lineCellModel_create(titleArr[i][2]);
+            model.zz_titleLeft(titleArr[i][0]);
+            model.zz_titleRight(titleArr[i][1]);
+            model.zz_cellHeight(CGFloatIn750(110));
+            model.zz_fontLeft([UIFont fontContent]);
+            
+            ZCellConfig *menuCellConfig = [ZCellConfig cellConfigWithClassName:[ZBaseLineCell className] title:model.cellTitle showInfoMethod:@selector(setModel:) heightOfCell:[ZBaseLineCell z_getCellHeight:model] cellType:ZCellTypeClass dataModel:model];
+            [weakSelf.cellConfigArr addObject:menuCellConfig];
+
+            if (i == 2) {
+                [weakSelf.cellConfigArr addObject:getGrayEmptyCellWithHeight(CGFloatIn750(20))];
+            }
+        }
+    }).zChain_block_setConfigDidSelectRowAtIndexPath(^(UITableView *tableView, NSIndexPath *indexPath, ZCellConfig *cellConfig) {
+        if ([cellConfig.title isEqualToString:@"weixin"]) {
+            UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+            pasteboard.string = @"xiangcenter";
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [ZAlertView setAlertWithTitle:@"已复制微信号到粘贴板" btnTitle:@"知道了" handlerBlock:^(NSInteger index) {
+                    
+                }];
+            });
+        }
+    });
+    
+    self.zChain_reload_ui();
+    [ZPublicTool settingCheckUpdateVersion];
 }
 
 #pragma mark lazy loading...
@@ -78,7 +79,6 @@
             make.width.height.mas_equalTo(CGFloatIn750(240));
         }];
         
-        
         UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         nameLabel.textColor = adaptAndDarkColor([UIColor colorTextBlack], [UIColor colorTextBlackDark]);
         nameLabel.text = @"似锦";
@@ -91,22 +91,6 @@
             make.top.equalTo(logoImageView.mas_bottom).offset(CGFloatIn750(28));
         }];
     }
-    
     return _topView;
 }
-
-
-#pragma mark tableView ------delegate-----
-- (void)zz_tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath cellConfig:(ZCellConfig *)cellConfig {
-    if ([cellConfig.title isEqualToString:@"weixin"]) {
-        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-        pasteboard.string = @"xiangcenter";
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [ZAlertView setAlertWithTitle:@"已复制微信号到粘贴板" btnTitle:@"知道了" handlerBlock:^(NSInteger index) {
-                
-            }];
-        });
-    }
-}
-
 @end
