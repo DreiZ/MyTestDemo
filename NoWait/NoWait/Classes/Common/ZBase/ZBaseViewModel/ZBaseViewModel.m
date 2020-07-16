4a2ad5ecfcc9d1f25dbdf9268b64aaada0d3d9be
zzz <zhang8520322@sina.com>
2020-07-16 17:45:07 +0800

    上传图片调整

--- NoWait/NoWait/Classes/Common/ZBase/ZBaseViewModel/ZBaseViewModel.m
+++ NoWait/NoWait/Classes/Common/ZBase/ZBaseViewModel/ZBaseViewModel.m
@@ -28,6 +28,54 @@
 
 
 + (void)uploadImageList:(NSDictionary *)params completeBlock:(resultDataBlock)completeBlock {
+//    @{@"type":@"8",@"imageKey":@{@"file":image}
+    NSMutableArray *tasklist = @[].mutableCopy;
+    if ([params objectForKey:@"imageKey"] && [params[@"imageKey"] isKindOfClass:[NSDictionary class]] && [params objectForKey:@"type"]) {
+        NSDictionary *tempDict = params[@"imageKey"];
+        if ([tempDict objectForKey:@"file"] && [tempDict[@"file"] isKindOfClass:[UIImage class]]) {
+            ZFileUploadDataModel *dataModel = [[ZFileUploadDataModel alloc] init];
+            dataModel.image = tempDict[@"file"];
+            dataModel.taskType = ZUploadTypeImage;
+            dataModel.taskState = ZUploadStateWaiting;
+            dataModel.type = params[@"type"];
+            [tasklist addObject:dataModel];
+        }
+    }
+    
+    if (!ValidArray(tasklist)) {
+        completeBlock(NO, @"没有要上传的图片");
+        return;
+    }
+    
+    [[ZFileUploadManager sharedInstance] asyncSerialUpload:tasklist progress:^(CGFloat p, NSInteger index) {
+        
+    } completion:^(id obj) {
+        if (obj && [obj isKindOfClass:[NSArray class]]) {
+            NSArray *arr = obj;
+            NSMutableArray *images = @[].mutableCopy;
+            for (int i = 0; i < arr.count; i++) {
+                if ([arr[i] isKindOfClass:[ZBaseNetworkBackModel class]]) {
+                    ZBaseNetworkBackModel *dataModel = arr[i];
+                    if (ValidDict(dataModel.data)) {
+                        ZBaseNetworkImageBackModel *imageModel = [ZBaseNetworkImageBackModel mj_objectWithKeyValues:dataModel.data];
+                        if ([dataModel.code integerValue] == 0 ) {
+                            [images addObject:SafeStr(imageModel.url)];
+                        }
+                    }
+                }else if([arr[i] isKindOfClass:[NSString class]]){
+                    [images addObject:SafeStr(arr[i])];
+                }
+            }
+            if (ValidArray(images)) {
+                completeBlock(YES, images[0]);
+                return;
+            }
+            
+            completeBlock(NO, @"上传的图片失败");
+        }
+    }];
+    
+    return;
     [ZNetworkingManager postImageServerType:ZServerTypeFile url:URL_file_v1_upload params:params completionHandler:^(id data, NSError *error) {
         ZBaseNetworkBackModel *dataModel = data;
         if (ValidDict(dataModel.data)) {
